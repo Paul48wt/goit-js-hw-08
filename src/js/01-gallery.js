abc716656ccd910e660bf5d81359bea5f5198635
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-19 18:27:20 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+// console.log(galleryMarkup);
+galleryContainer.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(galleryArray) {
+  return galleryArray
+    .map(
+      item => `<div class="gallery__item">
+      <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}"  title="${item.description}"/>
+    </a>
+</div>`
+    )
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
