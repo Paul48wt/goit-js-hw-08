c556452d63f3627ba11ef8b0aa6a95ba8205dd2e
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-19 22:06:58 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+import storage from './storage';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let currentTime = 0;
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player
+      .getCurrentTime()
+      .then(function (seconds) {
+        // seconds = the current playback position
+        // console.log(seconds);
+        // currentTime = seconds;
+        storage.save(currentTime, seconds);
+      })
+      .catch(function (error) {
+        // an error occurred
+      });
+  }, 1000)
+);
+
+player
+  .setCurrentTime(storage.load(currentTime))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
