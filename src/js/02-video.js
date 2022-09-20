70ea21c5330a37ee6b024a6e571f71c4ee7809f4
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-20 22:02:25 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,10 @@
 import Player from '@vimeo/player';
 import storage from './storage';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-let currentTime = 0;
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
 player.on(
   'timeupdate',
   throttle(() => {
@@ -12,9 +12,8 @@ player.on(
       .getCurrentTime()
       .then(function (seconds) {
         // seconds = the current playback position
-        // console.log(seconds);
-        // currentTime = seconds;
-        storage.save(currentTime, seconds);
+
+        storage.save(CURRENT_TIME_KEY, seconds);
       })
       .catch(function (error) {
         // an error occurred
@@ -23,7 +22,7 @@ player.on(
 );
 
 player
-  .setCurrentTime(storage.load(currentTime))
+  .setCurrentTime(storage.load(CURRENT_TIME_KEY))
   .then(function (seconds) {
     // seconds = the actual time that the player seeked to
   })
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,28 @@
+import storage from './storage';
+import throttle from 'lodash.throttle';
+const formData = {};
+const form = document.querySelector('.feedback-form');
+const btnSubmit = document.querySelector('button');
+const emailInput = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
 
+btnSubmit.addEventListener('click', onBtnSubmitClick);
+
+form.addEventListener('input', onFormTypeText);
+const FORM_KEY = 'feedback-form-state';
+
+function onFormTypeText(e) {
+  formData[e.target.name] = e.target.value;
+  storage.save(FORM_KEY, formData);
+}
+
+function populateTextarea() {
+  if (formData) {
+    console.log(storage.load(FORM_KEY));
+  }
+}
+
+function onBtnSubmitClick(e) {
+  e.preventDefault();
+  form.reset();
+}
