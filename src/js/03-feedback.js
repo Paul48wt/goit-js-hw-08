272d36bdf5ec83d8aba501f2f71e166a05e9d6d9
Pavlo Bieliak <66524818+Paul48wt@users.noreply.github.com>
2022-09-20 22:19:07 +0300

    03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,11 +5,12 @@ const form = document.querySelector('.feedback-form');
 const btnSubmit = document.querySelector('button');
 const emailInput = document.querySelector('input');
 const messageInput = document.querySelector('textarea');
-
+const FORM_KEY = 'feedback-form-state';
 btnSubmit.addEventListener('click', onBtnSubmitClick);
 
-form.addEventListener('input', onFormTypeText);
-const FORM_KEY = 'feedback-form-state';
+populateTextarea();
+
+form.addEventListener('input', throttle(onFormTypeText, 500));
 
 function onFormTypeText(e) {
   formData[e.target.name] = e.target.value;
